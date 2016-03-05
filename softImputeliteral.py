d1a955f963671a315b42350e7083b644daee9463
Lauren Dunlap <laurenwdunlap@ucla.edu>
2016-03-05 13:59:09 -0800

    fixed bugs in Ustep

--- softImputeliteral.py
+++ softImputeliteral.py
@@ -31,14 +31,15 @@ def loadMatrix(filename):
   #returns a coo_matrix X
   return X
 
-def Frob(Uold,Dsqold,Vold,U,Dsq,V):
-  denom=sum(Dsqold**2)
-  utu=Dsq* ((U.T).dot(Uold))
-  vtv=Dsqold* ((Vold.T).dot(V))
-  uvprod= sum(diag(utu.dot(vtv)))
-  num=denom+sum(Dsq**2) -2*uvprod
-  num = num/max(denom,1e-9)
-  return num
+def Frob(U,Dsqold,Vold,UtU,D_squared,VtV):
+    D_squared=np.diagflat(D_squared)
+    denom=np.trace(D_squared_old**2)
+    UtU=D_squared.dot((U.T.dot(U_old)))
+    VtV=D_squared_old.dot(V_old.T.dot(V))
+    UVproduct= np.trace(UtU.dot(VtV))
+    numerator=denom+np.trace(D_squared**2)-2*UVproduct
+    Delta_A=numerator/max(denom,10**(-100))
+    return num
   
 
 # Algorithm 3.1
@@ -81,16 +82,27 @@ def main():
   #Xt=X.transpose()
   #Omega is the <'list'> of coordinates with nonzero entries
 
+  # ----
+  
+  print "obtaining Omega"
+  X=X.todok()
+  Omega=X.keys()
+  all_keys=[]
+  for i in range(m):
+    for j in range(n):
+        all_keys.append((i,j))
+  all_keys_set=set(all_keys)
+  Omega_set=set(Omega)
+  complement_set=all_keys_set-Omega_set
+  complement=list(complement_set)
+  row=[tuple[0] for tuple in complement]
+  col=[tuple[1] for tuple in complement]
+
   X=X.todense()
   print("X todense")
   print(X)
 
-  # ----
-  xnas = [y == 0 for y in X ]
-  print("NA vals")
-  print(xnas)
-
-  nz=m*n-sum(xnas)
+  nz=m*n-len(complement)
   xfill = X
   #---
   print "setting threshold"
@@ -131,39 +143,36 @@ def main():
     print(np.shape(V))
     xhat=np.dot(U, Dsq.dot(V.T))
     #xfill[xnas]=xhat[xnas]
-    xfill[xnas]=xhat[xnas]
+    xfill[row, col]=xhat[row, col]
 
 
     ###The next line we could have done later; this is to match with sparse version
     # if(trace.it) obj=(.5*sum( (xfill-xhat)[!xnas]^2)+lambda*sum(Dsq))/nz
-    obj=(.5*sum((xfill-xhat)[xnas==False]**2)+Lambda*sum(d))/nz
+    #obj=(.5*sum((xfill-xhat)[complement]**2)+Lambda*sum(d))/nz
     
     ## V step
     
     #A=t(xfill%*%V)
-    A=(np.dot(xfill,V)).T
+    A=(xfill.dot(V)).T
     #if(lambda>0)A=A*(Dsq/(Dsq+lambda))
-    A=np.dot(A, np.divide(Dsq,(Dsq+Lambda)))
+    A=np.dot(np.divide(Dsq,(Dsq+Lambda)), A)
     #Asvd=svd(t(A))
-    U,d,V=svd(A.T)
+    u,d,v=linalg.svd(A.T, full_matrices=False)
     #U=Asvd$u
+    U=u
     #Dsq=Asvd$d
-    Dsq=np.diagflat(np.sqrt(d))
+    Dsq=np.diagflat(d)
     #V=V %*% Asvd$v
-    V=V.dot(linalg.svd(A,V))
+    V=V.dot(v)
     #xhat=U %*%(Dsq*t(V))
     xhat=U.dot(Dsq.dot(V.T))
     #xfill[xnas]=xhat[xnas]
-    xfill[xnas]=xhat[xnas]
+    xfill[row, col]=xhat[row, col]
     ratio=Frob(U.old,Dsq.old,V.old,U,Dsq,V)
-    ratio=np.linalg.norm('fro')
     #if(trace.it) cat(iter, ":", "obj",format(round(obj,5)),"ratio", ratio, "\n")
   
-      
-
     #plt.scatter(time.time()-t,Delta_B,c="red")
-    
-    
+  
     iterations+=1
     print "number of iterations: " +str(iterations)
     #if(iter==maxit)warning(paste("Convergence not achieved by",maxit,"iterations"))
@@ -171,7 +180,6 @@ def main():
     # plotting the convergence rate
     #plt.scatter(time.time()-t,Delta_A,c="blue")
     
-
     #we break the loop upon convergence
     #if((ratio > thresh)and(iter<maxit))
     if(ratio > threshold):
